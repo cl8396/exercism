d67fc904159fe178082fab2369d4d25fd344a413
Curtis <cl8396@gmail.com>
2023-12-22 19:06:27 +0000

    Complete RNA Transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRna = (input) => {
+  let result = [];
+  Array.from(input).forEach((letter) => {
+    switch (letter) {
+      case 'C':
+        result.push('G');
+        break;
+      case 'G':
+        result.push('C');
+        break;
+      case 'T':
+        result.push('A');
+        break;
+      case 'A':
+        result.push('U');
+        break;
+    }
+  });
+  return result.join('');
 };
